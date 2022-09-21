06a89e1b1428138a413bf6d27bc700d2ed5cf500
tofdz <80813116+tofdz@users.noreply.github.com>
2022-09-21 04:13:14 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 # Installateur theme xfce4
 # Tofdz 2022
-REP="XFCE-MOD"
 source ~/.config/user-dirs.dirs
 WDIR=$(pwd)
-cd $XDG_DOWNLOAD_DIR
-mkdir $REP
-cd $REP
-# Qojir Theme
+
+function themedownloader(){
 echo -e "==> [ 01 - 10 ]DL Qojir Theme"
-#wget https://codeload.github.com/vinceliuice/Qogir-theme/zip/refs/heads/master;
-git clone https://github.com/vinceliuice/Qogir-theme
-cd Qojir-theme
-./install.sh --tweaks round
-cd ..
+# Qojir Theme
+cd $HOME; git clone https://github.com/vinceliuice/Qogir-theme; cd Qojir-theme
+./install.sh --tweaks round;
 # Qojir Icon Theme
 echo -e "==> [ 02 - 10 ]DL Qojir Icon Theme"
-#wget https://codeload.github.com/vinceliuice/Qogir-icon-theme/zip/refs/heads/master;
-git clone https://github.com/vinceliuice/Qogir-icon-theme
-cd Qojir-icon-theme
+cd $HOME; git clone https://github.com/vinceliuice/Qogir-icon-theme; cd Qojir-icon-theme;
 ./install.sh 
-cd ..
-
+}
+function xbpsdep(){
 # DEPENDANCES & PAQUETS
 echo -e "==> VPM Install Apps"
 sudo vpm i -y  mugshot menulibre gtk2-engines gtk-engine-murrine libsass sassc ulauncher xfce4-docklike-plugin glava;
-# COPIER LE FICHIER dans xfce4 vers .config/xfce/xfce-panel/
-glava --copy-config
-ulauncher
+}
+function initapps(){
+glava --copy-config;
+ulauncher;
+exit
+}
+function dotfiles(){
+# POST INSTALL : DOT FILES !!!
+cd $WDIR/dot/
+pycp config/* $HOME/.config/
+pycp fonts/* $HOME/.fonts/
+exit
+}
+
+function main(){
+themedownloader
+xbpsdep
+dotfiles
+}
+
+main
